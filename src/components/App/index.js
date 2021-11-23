46a693eaf2f04e22d0fd020da6aa5ae74cebcb90
MichaelJosephMiller <michael.joseph.miller@gmail.com>
2021-11-23 16:30:51 -0500

    second commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,82 @@ import * as styles from './index.scss';
 function App() {
   return (
     <div id={styles.app}>
-      <h1>React Starter App</h1>
+      <div id={styles.display} className={styles.cell}>
+        <div id={styles.screen}>
+          <div id={styles.expression}>0,000,000.00</div>
+        </div>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.function}`}>
+          <sup>2</sup>&#8730;
+        </button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.function}`}>
+          <sup>y</sup>&#8730;x
+        </button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.function}`}>
+          x<sup>2</sup>
+        </button>
+      </div>
+      <div className={styles.cell}>
+        <button
+          id={styles.clear}
+          className={`${styles.btn} ${styles.function}`}
+        >
+          CLR
+        </button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>1</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>2</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>3</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>&#247;</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>4</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>5</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>6</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>x</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>7</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>8</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>9</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>-</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>0</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>+/-</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>=</button>
+      </div>
+      <div className={styles.cell}>
+        <button className={`${styles.btn} ${styles.main}`}>+</button>
+      </div>
     </div>
   );
 }
